d2a30d40aead77259185996abecfa164d5ca9faf
p schuhmacher <contact@pschuhmacher.com>
2026-04-29 16:47:24 +0200

    ajoute une ressource d'entrée a la racine, un middleware de gestion d'erreur

--- index.js
+++ index.js
@@ -5,17 +5,35 @@ const port = 3000;
 
 //Monte la swagger UI uniquement en environnement de dev
 if (process.env.NODE_ENV == "developement") {
-  const swaggerUi = require("swagger-ui-express");
-  const swaggerFile = require("./oad.json");
-  app.use("/v1/doc", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+    const swaggerUi = require("swagger-ui-express");
+    const swaggerFile = require("./oad.json");
+    app.use("/v1/doc", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 }
 
 const routerv1 = require("./router/v1");
+const routerHome = require('./router/v1/homeRouter')
 
 // Enregistrement routeur principal pour chaque version
+app.use("/", routerHome);
 app.use("/v1", routerv1);
+// Pour une éventuelle v2 de l'API
 // app.use('/v2', routerv2);
 
+// Dernier middleware, error handler
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
+
+    // On ne logue l'erreur complète que si on est en développement
+    if (process.env.NODE_ENV === "development") {
+        console.error(err.stack);
+    }
+    // render the error page
+    res.status(err.status || 500);
+    res.send("Error");
+});
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+    console.log(`web API Ticketing system listening on port ${port}`);
 });
